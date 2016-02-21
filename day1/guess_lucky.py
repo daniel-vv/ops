dc55929fb7414294c61b9a4b5ce258a3345edb11
unknown <>
2016-02-21 23:17:52 +0800

    update day1 scripts

--- day1/guess_lucky.py
+++ day1/guess_lucky.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+
+'''
+猜lucky number，n = 6
+猜的数字比6大，提示说你打印的小一点
+猜的数字比6小，提示说你打印的大一点
+猜的数字等于6，提示Bingo
+
+
+lucky_num = 6
+input_num = int(input('Please input your lucky num: ').strip())
+if input_num == lucky_num:
+    print('Bingo!')
+elif input_num > lucky_num:
+    print('The real nums is bigger!')
+else:
+    print('The real nums is smaller!')
+
+#循环
+lucky_num = 6
+input_num = 1
+while lucky_num != input_num:
+    input_num = int(input('Please input your lucky num: ').strip())
+    #if input_num == lucky_num:
+        #print('Bingo!')
+        #break
+    if input_num > lucky_num:
+        print('The real nums is bigger!')
+    elif input_num < lucky_num:
+        print('The real nums is smaller!')
+print('Bingo!')
+
+'''
+
 lucky_num = 30
 count = 0
-
 while count < 3:
     input_num = int(input('Please input your lucky number?'))
     if input_num < lucky_num:
@@ -14,6 +46,6 @@ while count < 3:
         print('Bingo')
         break
     count += 1
-else:
-    print('Too many errors to try!\n Bye!')
+else:     #当上面的条件不满足之后，就走这块，上面的循环没有被正常退出，else不会执行；
+    print('Too many errors to try!\nBye!')
 
---
+++ day1/testinput.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+
+name = input('Please input your name:').strip()
+age = int(input('Age:'))
+sex = input('Sex:').strip()
+print('Name: [%s]\nAge: [%s]\nSex: [%s]' % (name,age,sex))
