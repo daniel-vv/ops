625ae27fb5023fab6435f032ff8a218b5e0ddd9d
unknown <>
2016-03-18 23:57:18 +0800

    update day 1 work

--- day1/login/login.py
+++ day1/login/login.py
@@ -4,28 +4,44 @@
 
 def deny_user(username):
     print('您的用户已被锁定,请联系管理员')
-    f = open('blanklist','w+')
+    f = open('locked','w+')
     f.write(username)
     f.close()
+'''
+def Locked(username,input_user):
+定义一个判断用户是否锁定的函数
+    with open('locked','a') as Locked_info:
+        for i in Locked_info.readlines():
+            if i.strip() == username:
+                print('抱歉，%s已经锁定' %s username)
+                break
+'''
 
 
-count = 0
-while count < 3:
-    user_input_user = input('\033[32;1m请输入你的用户名:\033[0m').strip()
+Count = 0
+Flag = False
+while Count < 3:
+    user_input_user = str(input('\033[32;1m请输入你的用户名:\033[0m')).strip()
     # 判断用户是否存在黑名单内,如在直接拒绝,否则的话,继续下面认证判断
-    if user_input_user in blanklist:
-        print('您的账户信息尝试错误超限,请联系管理员解锁')
+    #Locked user_input_user
+    with open('locked','r') as Locked_info:
+        for i in Locked_info.readlines():
+            if i.strip() == user_input_user:
+                print('抱歉，%s已经锁定,请联系管理员，谢谢' % user_input_user)
+                Flag = True
+                break
+    if not Flag:
+        user_input_password = str(input('\033[32;1m请输入密码：\033[0m')).strip()
+        with open('passwd','r') as f:
+            for u in f.readlines():
+                if u.split(':')[0] == user_input_user: #and u.split(':')[1] == user_input_password:
+                    print('哇，登录成功，欢迎来到这里')
+                    Flag = True
+                    break
+    if Flag:
         break
-    User = fuser.read()
-    if user_input_user in User:
-        user_input_passwd = input('请输入你的密码:').strip()
-        #User = f.readlines()
-        #if user_input_user == ''
     else:
-        print('账户不存在,或者密码不正确!')
-        count += 1
         continue
-    count += 1
 
 
 
