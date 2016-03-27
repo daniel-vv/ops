0d06d2a21dbf7e7102d6ab5d5d2acd997dbf78b0
unknown <>
2016-03-27 21:20:49 +0800

    update day2

---
+++ day2/init_exer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+i = 23
+#result = i.bit_length()  #返回表示该数字的二进制占位最少位数
+#result = i.conjugate() #返回该复数的共轭复数
+#result = i.__abs__()  #绝对值
+#result = i.__add__(3) #加
+#result = i.__truediv__(3) #除，x/y
+#result = i.__divmod__(2)  #相除，得到商和余数组成的元组
+#result = i.__floordiv__(3)  #x//y
+#result = i.__float__() #转换为浮点
+#result = i.__int__()   #转换为整数
+#result = i.__invert__()  #反转 -(x+1)
+#result = i.__lshift__(2)  #左移
+#result = i.__mod__(3) #余
+#result = i.__mul__(3) #乘积
+#result = i.__pow__(4)  #幂，次方
+#result = i.__radd__(4)  #y+x
+#result = i.__rand__(5) #y&x
+#result = i.__repr__() #转换为解释器可以读取的形式
+#result = i.__str__() #转为人阅读的形式，没有适用于人阅读形式的话，则返回阅读器阅读的形式
+#result = i.__rfloordiv__(50)   #y//x
+#result = i.__rsub__(3)  #y-x
+#result = i.__rtruediv__(5)  #y/x
+#result = i.__rxor__(10) #y^x
+#result = i.__xor__(10) #x^y
+#result = i.denominator #分母=1
+#result = i.imag  #虚数，没有意义
+#result = i.numerator  #分子=数字大小
+#result = i.real #实数，无意义
+print(result)
